649ac1f175889bbdd402c65ba5e4b2d0f414d08b
Dmytro Krasavin <krasavin.d.96@gmail.com>
2024-09-12 17:30:32 +0300

    Add ByteUtils.modifyBit method

--- src/main/java/com/jbroker/utils/ByteUtils.java
+++ src/main/java/com/jbroker/utils/ByteUtils.java
@@ -6,8 +6,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE) // This is the utility class
 public class ByteUtils {
 
-  public static boolean isBitSet(byte value, int position) {
-    return ((value >> position) & 1) == 1;
+  public static boolean isBitSet(byte byteValue, int position) {
+    return ((byteValue >> position) & 1) == 1;
+  }
+
+  public static byte modifyBit(byte byteValue, int bitPosition, boolean setBit) {
+    if (bitPosition < 0 || bitPosition > 7) {
+      throw new IllegalArgumentException("Bit position must be between 0 and 7");
+    }
+    // Create a mask with the bit at the specified position set to 1
+    byte mask = (byte) (1 << bitPosition);
+
+    if (setBit) {
+      // Set the bit using bitwise OR operation
+      return (byte) (byteValue | mask);
+    }
+    // Clear the bit using bitwise AND operation with the inverse of the mask
+    return (byte) (byteValue & ~mask);
   }
 
   public static int toUnsigned(byte value) {
