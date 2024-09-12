8c93af0c5300779ed0dbfb23ec33a2128450bb79
Dmytro Krasavin <krasavin.d.96@gmail.com>
2024-09-12 17:34:24 +0300

    Change logging in PacketReader

--- src/main/java/com/jbroker/packet/reader/PacketReader.java
+++ src/main/java/com/jbroker/packet/reader/PacketReader.java
@@ -1,43 +1,38 @@
 package com.jbroker.packet.reader;
 
 import com.jbroker.command.CommandType;
+import com.jbroker.logger.Logger;
 import com.jbroker.packet.FixedHeader;
 import com.jbroker.packet.MqttPacket;
-import com.jbroker.packet.decoder.impl.PingReqPacketDecoder;
 import com.jbroker.packet.decoder.impl.ConnectPacketDecoder;
 import com.jbroker.packet.decoder.impl.DisconnectPacketDecoder;
+import com.jbroker.packet.decoder.impl.PingReqPacketDecoder;
 import java.io.IOException;
 import java.io.InputStream;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class PacketReader {
 
+  private final Logger log = Logger.getInstance();
   private final FixedHeaderReader fixedHeaderReader;
   private final ConnectPacketDecoder connectDecoder;
   private final PingReqPacketDecoder pingReqDecoder;
   private final DisconnectPacketDecoder disconnectDecoder;
-
-  public PacketReader(
-      FixedHeaderReader fixedHeaderReader,
-      ConnectPacketDecoder connectDecoder,
-      PingReqPacketDecoder pingReqDecoder,
-      DisconnectPacketDecoder disconnectDecoder) {
-    this.fixedHeaderReader = fixedHeaderReader;
-    this.connectDecoder = connectDecoder;
-    this.pingReqDecoder = pingReqDecoder;
-    this.disconnectDecoder = disconnectDecoder;
-  }
 
   public MqttPacket read(InputStream input) throws IOException {
     FixedHeader fixedHeader = fixedHeaderReader.read(input);
     byte[] packetBuffer = buildPacketBuffer(input, fixedHeader.remainingLength());
 
     CommandType commandType = CommandType.resolveType(fixedHeader.controlPacketType());
+    log.info("%s packet received from client", commandType.name());
+
     return switch (commandType) {
       case CONNECT -> connectDecoder.decode(fixedHeader, packetBuffer);
       case PINGREQ -> pingReqDecoder.decode(fixedHeader, packetBuffer);
       case DISCONNECT -> disconnectDecoder.decode(fixedHeader, packetBuffer);
       default -> throw new IllegalArgumentException(
-          "Could not find applicable packet parser for command type: " + commandType.name());
+          "Could not find applicable packet encoder for command type: " + commandType.name());
     };
   }
 
