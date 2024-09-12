445fd01be7ee90b98dc2090b651d7a6dd109d6c8
Dmytro Krasavin <krasavin.d.96@gmail.com>
2024-09-12 17:29:18 +0300

    Add ArrayUtils class

---
+++ src/main/java/com/jbroker/utils/ArrayUtils.java
@@ -0,0 +1,25 @@
+package com.jbroker.utils;
+
+import java.util.Arrays;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE) // This is the utility class
+public class ArrayUtils {
+
+  public static byte[] mergeArrays(byte[]... arrays) {
+    int totalLength = Arrays.stream(arrays)
+        .mapToInt(array -> array.length)
+        .sum();
+
+    byte[] resultArray = new byte[totalLength];
+    int resultArrayIndex = 0;
+    for (byte[] array : arrays) {
+      for (byte element : array) {
+        resultArray[resultArrayIndex] = element;
+        resultArrayIndex++;
+      }
+    }
+    return resultArray;
+  }
+}
