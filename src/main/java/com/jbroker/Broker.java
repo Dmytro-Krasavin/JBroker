245acee30f8e08eb3bcfde8aca5df6b70b4a2323
Dmytro Krasavin <krasavin.d.96@gmail.com>
2024-09-29 21:35:20 +0300

    Delegate manual Thread creating to ExecutorService

--- src/main/java/com/jbroker/Broker.java
+++ src/main/java/com/jbroker/Broker.java
@@ -6,27 +6,39 @@ import com.jbroker.message.queue.MessageQueueProcessor;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import lombok.RequiredArgsConstructor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RequiredArgsConstructor
 public class Broker {
 
   private final ClientConnectionManager clientConnectionManager;
   private final MessageQueueProcessor messageQueueProcessor;
+  private final ExecutorService connectionExecutor;
+  private final ExecutorService messageProcessingExecutor;
 
+  public Broker(ClientConnectionManager clientConnectionManager,
+      MessageQueueProcessor messageQueueProcessor) {
+    this.clientConnectionManager = clientConnectionManager;
+    this.messageQueueProcessor = messageQueueProcessor;
+    this.connectionExecutor = Executors.newCachedThreadPool();
+    this.messageProcessingExecutor = Executors.newSingleThreadExecutor();
+  }
+
+  /**
+   * Starts the MQTT broker, listening on the provided port for incoming client connections.
+   */
   public void run(int port) {
-    new Thread(messageQueueProcessor).start();
+    messageProcessingExecutor.submit(messageQueueProcessor);
 
     try (ServerSocket serverSocket = new ServerSocket(port)) {
       log.info("MQTT Broker is listening on port {}", port);
 
       while (true) {
-        Socket socket = serverSocket.accept();
-        // Handle client communication in a separate thread
-        ClientConnection connection = clientConnectionManager.createClientConnection(socket);
-        new Thread(connection).start();
+        Socket clientSocket = serverSocket.accept();
+        ClientConnection connection = clientConnectionManager.createClientConnection(clientSocket);
+        connectionExecutor.submit(connection);
       }
     } catch (IOException e) {
       log.error("IOException occurred: {}", e.getMessage());
